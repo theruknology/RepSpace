071e69740e29bf9b3c3b8839a777a2e9b8fc4a92
Mohammed Ruknuddin <iosrukn123@gmail.com>
2022-12-19 22:33:48 +0300

    project-setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rep Space
+A study companion and scheduling application, that utilizes various concepts of spaced repitition, to plan revision and study sessions more effeciently.
---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./index.css";
+
+export default function App() {
+  return <h1 className="text-primary text-4xl font-bold">Tailwind Testing</h1>;
+}
